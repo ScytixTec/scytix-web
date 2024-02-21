2f78870a88ff4eb2307641e8522f3e6f1e8e9b35
NaskoTrenchevScytix <160243914+NaskoTrenchevScytix@users.noreply.github.com>
2024-02-21 13:12:34 +0200

    Creating controllers and routes (#5)
    * Creating controllers and routes
    
    * Applying changes as per the comments
    
    * Fixing imports

---
+++ main/api/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+import routes from "./routes";
+
+const app = express();
+
+app.use("/api", routes);
+
+app.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
