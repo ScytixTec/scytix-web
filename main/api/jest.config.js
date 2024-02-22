1b5474e101242f46fe6e0d87e1e1b56adab10015
NaskoTrenchevScityx <nasko@scytix.com>
2024-02-22 12:24:49 +0200

    Removing js types and the pattern, adding supertest

--- main/api/jest.config.js
+++ main/api/jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+    "**/__tests__/**/*.+(ts|tsx)",
   ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
