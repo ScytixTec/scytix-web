f14cd073b511229001ed57838dd83e9ef460f060
NaskoTrenchevScityx <nasko@scytix.com>
2024-02-22 17:02:51 +0200

    Adjusting the tests, applying clearMocks: true

--- main/api/src/__tests__/status.test.ts
+++ main/api/src/__tests__/status.test.ts
@@ -2,13 +2,15 @@ import { Request, Response } from "express";
 
 import { getStatus, getStatusById } from "../controllers/status";
 
+const req = {} as unknown as Request;
+const res = {
+  send: jest.fn(),
+  status: jest.fn(() => res),
+  sendStatus: jest.fn(),
+} as unknown as Response;
+
 describe("get status", () => {
   it("should return the correct response", () => {
-    const req = {} as Request;
-    const res = {
-      send: jest.fn(),
-      status: jest.fn(() => res),
-    } as unknown as Response;
     getStatus(req, res);
     expect(res.send).toHaveBeenCalledTimes(1);
     expect(res.send).toHaveBeenCalledWith({
@@ -21,20 +23,12 @@ describe("get status", () => {
 describe("get status with id", () => {
   it("Should return an error for invalid paths", () => {
     const req = { params: { id: "test" } } as unknown as Request;
-    const res = {
-      send: jest.fn(),
-      sendStatus: jest.fn(),
-    } as unknown as Response;
     getStatusById(req, res);
     expect(res.sendStatus).toHaveBeenCalledWith(404);
   });
 
   it("Should return the right Id when called with number", () => {
     const req = { params: { id: 1 } } as unknown as Request;
-    const res = {
-      send: jest.fn(),
-      status: jest.fn(() => res),
-    } as unknown as Response;
     getStatusById(req, res);
     expect(res.send).toHaveBeenCalledTimes(1);
     expect(res.send).toHaveBeenCalledWith({
