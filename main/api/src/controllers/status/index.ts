56cb9e65d71502fee6460bec2e396655edad2a3b
NaskoTrenchevScityx <nasko@scytix.com>
2024-02-23 10:17:59 +0200

    trying to use jest-when

--- main/api/src/controllers/status/index.ts
+++ main/api/src/controllers/status/index.ts
@@ -1,15 +1,17 @@
+import { StatusCodes } from "http-status-codes";
+
 import { BasicControllerType } from "../../types";
 
 export const getStatus: BasicControllerType = (req, res) => {
-  res.status(200).send({
+  res.status(StatusCodes.OK).send({
     status: "OK",
   });
 };
 
 export const getStatusById: BasicControllerType = (req, res) => {
-  if (isNaN(+req.params.id)) return res.sendStatus(404);
+  if (isNaN(+req.params.id)) return res.sendStatus(StatusCodes.NOT_FOUND);
 
-  res.status(200).send({
+  res.status(StatusCodes.OK).send({
     statusId: req.params.id,
   });
 };
