a6db0319de46ab062a454539209ab61b7bf8e4e0
NaskoTrenchevScityx <nasko@scytix.com>
2024-02-25 09:21:51 +0200

    Applying fix to second batch of the comments

--- modules/urn/src/index.ts
+++ modules/urn/src/index.ts
@@ -13,7 +13,7 @@ type UrnResourceValue = (typeof UrnResource)[keyof typeof UrnResource];
 export const validateUrn = (
   urn: string,
   resource: UrnResourceValue,
-): Boolean => {
+): boolean => {
   if (UrnResource.hasOwnProperty(resource)) {
     const pattern = new RegExp(`^urn:${urnNId}:${resource}:[a-zA-Z0-9_-]+$`);
     return pattern.test(urn);
@@ -22,22 +22,23 @@ export const validateUrn = (
   return false;
 };
 
-export const createUrn = (
-  resource: UrnResourceValue,
-  id: string,
-): undefined => {};
+export const createUrn = (resource: UrnResourceValue, id: string): string => {
+  return `urn:scytix:${resource}:${id}`;
+};
 
-export const getUrnResource = (urn: string): string | undefined => {
-  const UrnResource = urn.split(":")[2];
+export const getUrnResource = (urn: string): UrnResourceValue | undefined => {
+  const urnResource = urn.split(":")[2] as UrnResourceValue;
 
-  if (UrnResource) {
-    return UrnResource;
+  if (urnResource) {
+    if (validateUrn(urn, urnResource)) {
+      return urnResource;
+    }
   }
 };
 
 export const getUrnId = (urn: string): string | undefined => {
-  const UrnId = urn.split(":")[3];
-  if (UrnId) {
-    return UrnId;
+  const urnId = urn.split(":")[3];
+  if (urnId) {
+    return urnId;
   }
 };
