c6bcb0f8b5d421cd555f0857b52e7e8b49f43ed5
NaskoTrenchevScityx <nasko@scytix.com>
2024-02-26 13:06:44 +0200

    STX-18-creating-config-for-api

--- main/api/src/config.ts
+++ main/api/src/config.ts
@@ -1,5 +1,18 @@
 import { z } from "zod";
+import yaml from "yaml";
+import fs from "fs";
+import * as packageJson from "../package.json";
+
+const configYaml = fs.readFileSync("./config.yml", "utf8");
 
 const ConfigSchema = z.object({
   version: z.string(),
 });
+
+type Config = z.infer<typeof ConfigSchema>;
+const version = packageJson.version;
+
+const parsedConfig = ConfigSchema.parse(yaml.parse(configYaml));
+parsedConfig.version = version;
+
+export const config: Config = parsedConfig;
--- main/api/src/controllers/status/index.ts
+++ main/api/src/controllers/status/index.ts
@@ -1,9 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 
 import { BasicControllerType } from "../../types";
+import { config } from "../../config";
 
 export const getStatus: BasicControllerType = (req, res) => {
-  res.status(StatusCodes.OK).send({
-    status: "OK",
-  });
+  res.status(StatusCodes.OK).send(config.version);
 };
