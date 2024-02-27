f9a2481e635ca740b2f5a2589b758f6a987f27d0
NaskoTrenchevScityx <nasko@scytix.com>
2024-02-27 17:47:35 +0200

    Adding cognito

---
+++ main/admin-panel/src/config.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+import * as packageJson from "../package.json";
+import YamlConfig from "../config.yml";
+
+const ConfigSchema = z.object({
+  version: z.string(),
+  cognito: z.object({
+    userPoolId: z.string(),
+    appClientId: z.string(),
+  }),
+});
+
+export const config = ConfigSchema.parse({
+  ...YamlConfig,
+  version: packageJson.version,
+});
