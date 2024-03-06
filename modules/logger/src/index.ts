31445a449f55dfb8145ea719eeaf8d684a21ce88
Asen Bozhilov <asen@scytix.com>
2024-03-06 22:50:56 +0000

    STX-20: add winston logger

---
+++ modules/logger/src/index.ts
@@ -0,0 +1,83 @@
+import { randomUUID } from "crypto";
+import { createLogger, format, transports } from "winston";
+import WinstonCloudwatch = require("winston-cloudwatch");
+
+export type LoggerConfig =
+  | {
+      type: "local";
+      level: string;
+    }
+  | {
+      type: "aws";
+      level: string;
+      region: string;
+      serviceName: string;
+      stage: string;
+      version: string;
+    };
+
+export interface LoggerMessage {
+  message: string;
+}
+
+interface ScytixLogger {
+  debug: (data: LoggerMessage) => void;
+  error: (data: LoggerMessage) => void;
+  info: (data: LoggerMessage) => void;
+  silly: (data: LoggerMessage) => void;
+  warn: (data: LoggerMessage) => void;
+  close: () => void;
+}
+
+let logger: ScytixLogger;
+
+export const initLogger = (config: LoggerConfig): ScytixLogger => {
+  const winstonLogger = createLogger({
+    level: config.level,
+    format: format.json(),
+  });
+
+  switch (config.type) {
+    case "local":
+      winstonLogger.add(
+        new transports.Console({
+          handleExceptions: true,
+        }),
+      );
+      break;
+
+    case "aws":
+      winstonLogger.add(
+        new WinstonCloudwatch({
+          awsRegion: config.region,
+          logGroupName: `${config.serviceName}-${config.stage}`,
+          logStreamName: `${config.version}-${randomUUID()}`,
+        }),
+      );
+      break;
+  }
+
+  const loggerHelper =
+    <T extends Function>(method: T) =>
+    (data: LoggerMessage) =>
+      method(data);
+
+  logger = {
+    debug: loggerHelper(logger.debug),
+    error: loggerHelper(logger.error),
+    info: loggerHelper(logger.info),
+    silly: loggerHelper(logger.silly),
+    warn: loggerHelper(logger.warn),
+    close: () => logger.close(),
+  };
+
+  return logger;
+};
+
+export const getLogger = (): ScytixLogger => {
+  if (!logger) {
+    throw new Error("Logger is not initialized");
+  }
+
+  return logger;
+};
